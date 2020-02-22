9994c14293cdd11c203476f494ef269b1285c5b1
vitor <vitorhunterwe@gmail.com>
2020-02-21 22:20:29 -0300

    testes 1

---
+++ teste com programa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace teste_com_programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ola mundo!");
+        }
+    }
+}
